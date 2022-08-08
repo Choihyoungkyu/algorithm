0d339f79ab4d276a2209b63a619c6fde07c71d18
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2022-08-09 01:08:36 +0900

    [Silver V] Title: 수 이어가기, Time: 88 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2635. 수 이어가기/수 이어가기.py
+++ 백준/Silver/2635. 수 이어가기/수 이어가기.py
@@ -1,28 +1,23 @@
-import sys
+# 104ms
 
+import sys
 input = lambda: sys.stdin.readline().strip()
 x= int(input())
 
 cnt = 0
-for i in range(int(x//2), x+1):
+for i in range(int(x//2), x+1): # x//2부터 시작인 이유 : 절반보다 작으면 바로 다음에서 끝남
     num = [x]
     num.append(i)
     j = 0
     tmp = 0
-    while num[-1] >= 0:
+    while num[-1] >= 0:  # 음수가 나올때까지 반복
         num.append(num[j] - num[j+1])
         j += 1
     tmp = len(num)
-    if tmp > cnt:
+    if tmp > cnt:  # cnt가 개선될때마다 idx 저장
         cnt = len(num) - 1
-        idx = i
+        lst = num
 print(cnt)
-num = [x]
-num.append(idx)
-j = 0
-while num[-1] >= 0:
-    num.append(num[j] - num[j+1])
-    j += 1
-num.pop()
-for i in num:
+lst.pop()
+for i in lst:
     print(i, end=' ')
