6902bb9238de761fb67dcf5676a0434765dd4240
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2022-08-22 02:31:10 +0900

    [Gold III] Title: 크게 만들기, Time: 524 ms, Memory: 37428 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.py
@@ -0,0 +1,32 @@
+import sys
+input = lambda: sys.stdin.readline().strip()
+
+N, K = map(int, input().split())
+num = list(map(int, input()))
+
+stack = [num[0]]
+cnt = 0
+flag = False
+
+for i in range(1, N):
+    if i == K + len(stack) and len(stack) != K:
+        for j in range(i,N):
+            stack.append(num[j])
+            flag = True
+    
+    if flag:
+        break
+
+    while True:
+        if len(stack) == 0:
+            break
+        elif stack[-1] < num[i] and i != K + len(stack):
+            stack.pop()
+        else:
+            break
+
+    stack.append(num[i])
+        
+        
+for i in range(N-K):
+    print(stack[i], end='')
