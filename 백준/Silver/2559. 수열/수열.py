fec027db9c2e226734969645780f2444c36b840d
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2022-08-21 21:21:05 +0900

    [Silver III] Title: 수열, Time: 112 ms, Memory: 39572 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.py
@@ -0,0 +1,14 @@
+import sys
+input = lambda: sys.stdin.readline().strip()
+
+N, K = map(int, input().split())
+lst = list(map(int, input().split()))
+maxV = sum(lst[0:K])
+tmp = maxV
+
+for i in range(K, N):
+    tmp = tmp - lst[i-K] + lst[i]
+    if maxV < tmp:
+        maxV = tmp
+
+print(maxV)
