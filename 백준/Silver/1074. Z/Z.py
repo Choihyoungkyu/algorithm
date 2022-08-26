1447ce60a95363c2071169f0fcfc02063807a989
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2022-08-26 15:42:11 +0900

    [Silver I] Title: Z, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -3,26 +3,26 @@ input = lambda : sys.stdin.readline().strip()
 
 def f(N, ni1, nj1):
     global cnt
-    if N == 1:
+    if N == 1:                                      # Z로 수를 늘릴 수 있다면 (=영역을 나눌 필요가 없다면)
         cnt -= 1
-        for i in range(2):
+        for i in range(2):                          # Z 순서대로 카운트 증가
             for j in range(2):
                 cnt += 1
-                if ni1+i == r and nj1+j == c:
+                if ni1+i == r and nj1+j == c:       # 목표위치 도달 시 cnt값 반환
                     return cnt
-    else:
-        ni1, ni2 = ni1, ni1 + (2**N) - (2**(N-1))
+    else:                                           # 영역을 나눠야 한다면
+        ni1, ni2 = ni1, ni1 + (2**N) - (2**(N-1))   # 영역의 중앙을 기준으로 나눔
         nj1, nj2 = nj1, nj1 + (2**N) - (2**(N-1))
-        N -= 1
-        if r < ni1 + 2**N and c < nj1 + 2**N:
+        N -= 1                                      # 영역 축소
+        if r < ni1 + 2**N and c < nj1 + 2**N:       # 원래 영역의 2사분면일 경우
             f(N, ni1, nj1)
-        elif r < ni1 + 2**N and c >= nj1 + 2**N:
+        elif r < ni1 + 2**N and c >= nj1 + 2**N:    # 원래 영역의 1사분면일 경우
             cnt += ((2**N)**2)
             f(N, ni1, nj2)
-        elif r >= ni1 + 2**N and c < nj1 + 2**N:
+        elif r >= ni1 + 2**N and c < nj1 + 2**N:    # 원래 영역의 3사분면일 경우
             cnt += ((2**N)**2) * 2
             f(N, ni2, nj1)
-        elif r >= ni1 + 2**N and c >= nj1 + 2**N:
+        elif r >= ni1 + 2**N and c >= nj1 + 2**N:   # 원래 영역의 4사분면일 경우
             cnt += ((2**N)**2) * 3
             f(N, ni2, nj2)
 
