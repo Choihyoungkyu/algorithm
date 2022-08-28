e47f28c5ee66d8180e4049acff37cb9f4ddf38b5
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2022-08-28 23:45:19 +0900

    [Gold V] Title: 토마토, Time: 2420 ms, Memory: 114016 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,49 @@
+import sys
+from collections import deque
+
+input = lambda: sys.stdin.readline().strip()
+
+def DFS(lst):
+    global cnt
+    visited = deque([0] * M for _ in range(N))
+    # visited = [[0] * M for _ in range(N)]
+    D = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+    lst2 = deque()
+    tmp = 0
+    queue = deque()
+    for i in range(N):
+        for j in range(M):
+            if lst[i][j] == 1:
+                queue.append([i, j])
+                visited[i][j] = 1
+            elif lst[i][j] == 0:
+                tmp += 1
+            else:
+                visited[i][j] = -1
+    if tmp == 0 and len(lst2) != 0:
+        return 0
+
+    tmp = deque()
+    while queue:
+        ni, nj = queue.popleft()
+        for di, dj in D:
+            if 0<=ni+di<N and 0<=nj+dj<M and lst[ni+di][nj+dj]==0 and visited[ni+di][nj+dj] == 0:
+                tmp.append([ni+di, nj+dj])
+                visited[ni+di][nj+dj] = cnt
+            
+        if queue == deque() and tmp != deque():
+            queue = tmp
+            cnt += 1
+            tmp = deque()
+
+    for i in visited:
+        if 0 in i:
+            return -1
+    else:
+        return cnt-1
+
+
+M, N = map(int, input().split())
+lst = [list(map(int, input().split())) for _ in range(N)]
+cnt = 1
+print(DFS(lst))
