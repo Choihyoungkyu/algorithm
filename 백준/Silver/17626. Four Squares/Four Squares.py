a0dba94c54d9fddbdeaddb124b6ccafa0d757a96
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2022-08-31 23:40:56 +0900

    [Silver III] Title: Four Squares, Time: 128 ms, Memory: 31352 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -2,14 +2,22 @@ import sys
 input = lambda : sys.stdin.readline().strip()
 
 N = int(input())
-DP = [0] * (N+1)
-DP[0] = 0
-for i in range(1, N+1):
-    tmp = 100
-    for j in range(1, i+1):
-        if j*j > i:
-            break
-        if DP[i - j*j] + 1 < tmp:
-            tmp = DP[i - j*j] + 1
-    DP[i] = tmp
-print(DP[N])
+lst1 = [i*i for i in range(1, int(N**0.5)+1)]
+lst2 = []
+for i in range(len(lst1)):
+    for j in range(i, len(lst1)):
+        lst2.append(lst1[i]+lst1[j])
+lst2_set = set(lst2)
+
+def answer(N):
+    if N in lst1:  # 제곱수면
+        return 1
+    elif N in lst2:  # 제곱수 두개를 더해서 만들 수 있는 수면
+        return 2
+    else:
+        for square in lst1:  # 제곱 수 중
+            if N - square in lst2:  # n에서 제곱수를 뺀 수가 제곱수 두개를 더해서 만들수 있는 수면
+                return 3
+    return 4
+
+print(answer(N))
