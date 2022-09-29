212b93514c2f89dd2afdbea423b70322f117e31d
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2022-09-29 16:18:20 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 2,352 ms, Memory: 204,720 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.py
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.py
@@ -1,33 +1,32 @@
 def leng(x1, x2, y1, y2):
     return (x2-x1)**2 + (y2-y1)**2
 
-def prim(r, V):
-    MST = [0] * (V)
-    MST[r] = 1
-    s = 0
-    for _ in range(V-1):
-        u = r
-        minV = 1000000*1000000
-        for i in range(V):
-            if MST[i] == 1:
-                for j, w in adjL[i]:
-                    if MST[j] == 0 and minV > w:
-                        u = j
-                        minV = w
-        s += minV
-        MST[u] = 1
-    return s
+def find_set(x):
+    while x != rep[x]:
+        x = rep[x]
+    return x
+
+def union(x, y):
+    rep[find_set(y)] = find_set(x)
 
 for tc in range(1, int(input())+1):
     N = int(input())
     X = list(map(int, input().split()))
     Y = list(map(int, input().split()))
     E = float(input())
-    adjL = [[] for _ in range(N)]
+    edge = []
     for i in range(N-1):
         for j in range(i+1, N):
-            adjL[i].append([j, E * leng(X[i], X[j], Y[i], Y[j])])
-            adjL[j].append([i, E * leng(X[i], X[j], Y[i], Y[j])])
-    # for idx, i in enumerate(adjL):
-    #     print(idx, i)
-    print(f'#{tc} {int(round(prim(0, N), 0))}')
+            edge.append([i, j, E * leng(X[i], X[j], Y[i], Y[j])])
+    edge.sort(key=lambda x:x[2])
+    rep = [i for i in range(N)]
+    cnt = 0
+    total = 0
+    for u, v, w in edge:
+        if find_set(u) != find_set(v):
+            cnt += 1
+            union(u, v)
+            total += w
+            if cnt == N-1:
+                break
+    print(f'#{tc} {int(round(total, 0))}')
