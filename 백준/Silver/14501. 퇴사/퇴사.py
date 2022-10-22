4fc06050471b51ea3b9d9f595663488380c561cc
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2022-10-22 18:38:53 +0900

    [Silver III] Title: 퇴사, Time: 88 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,20 @@
+import sys
+input = lambda:sys.stdin.readline().strip()
+
+N = int(input())
+lst = []
+for _ in range(N):
+    lst.append(list(map(int, input().split())))
+
+def f(i, tmp):
+    global maxV
+    if i < N:
+        if i + lst[i][0] <= N :
+            if lst[i][1] + tmp > maxV:
+                maxV = lst[i][1] + tmp
+            f(i+lst[i][0], tmp+lst[i][1])
+        f(i+1, tmp)
+
+maxV = 0
+f(0, 0)
+print(maxV)
