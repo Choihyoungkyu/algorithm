911af3d059d51e567e260d45402afeeebaf00698
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2022-11-09 13:52:31 +0900

    [level 2] Title: 양궁대회, Time: 0.87 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
@@ -1,47 +1,44 @@
 def solution(n, info):
     global maxV, maxS, idx
     answer = []
-    maxV = 0
-    maxS = []
-    idx = 0
-
-    # 가장 낮은 점수를 더 많이 맞힌 경우 구현하기
-    # maxV < tot랑 maxV == tot 나눠서 구현하기
+    maxV = 0                        # 가장 큰 점수 차이
+    maxS = []                       # 라이언 과녁 정보
+    idx = 0                         # 가장 작은 점수를 맞힌 위치
     
     def f(i, tot, s, cnt):
         global maxV, maxS, idx
-        if i == 11:
-            if maxV < tot:
-                maxV = tot
-                s = list(map(int, s.strip().split()))
+        if i == 11:                 # 과녁에 활을 다 쏜 경우
+            if maxV < tot:          # 점수차를 갱신해야 할 경우
+                maxV = tot          # 점수차 갱신
+                s = list(map(int, s.strip().split()))   # 최대 점수차의 과녁 정보를 저장
                 maxS = s[:]
-                for j in range(len(maxS)-1, -1, -1):
+                for j in range(len(maxS)-1, -1, -1):    # 해당 과녁의 가장 작은 점수의 위치를 저장
                     if s[j]:
                         idx = j
-                        print(idx)
                         break
-            elif maxV == tot:
+            elif maxV == tot:                           # 점수차가 최대 점수차와 같은 경우
                 s = list(map(int, s.strip().split()))
                 if maxS:
-                    for j in range(len(maxS)-1, idx-1, -1):
+                    for j in range(len(maxS)-1, idx-1, -1): # 가장 작은 점수의 위치 비교 및 개수 비교 후 과녁 정보 저장
                         if s[j] > maxS[j]:
                             maxS = s[:]
                             break
                 return
-        else:
-            if cnt and cnt > info[i] :
+            
+        else:                                           # 과녁에 활을 쏘는 경우
+            if cnt and cnt > info[i] :                  # 라이언이 이기는 경우 : 어피치보다 +1개를 더 맞혀야 됨
                 f(i+1, tot+(10-i), s+str(info[i]+1)+' ', cnt-(info[i]+1))
-            if info[i]:
+            if info[i]:                                 # 어피치가 이기는 경우 : 화살 안씀
                 f(i+1, tot-(10-i), s+'0 ', cnt)
-            else:
+            else:                                       # 둘 다 0인 경우
                 f(i+1, tot, s+'0 ', cnt)
 
     f(0, 0, '', n)
     answer = maxS
     
-    if not answer or maxV == 0:
+    if maxV == 0:                         # 라이언이 이길 수 없는 경우 : maxV == 0 인 경우
         answer = [-1]
-    else:
+    else:                                               # 라이언이 이기는데 화살을 다 안쓰고 이기면 마지막 0점에 몰아주기
         cnt = 0
         for i in range(11):
             cnt += answer[i]
