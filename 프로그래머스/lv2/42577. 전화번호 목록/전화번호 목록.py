ca332fec2c576a748c62f61c72c6e513cab23f86
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2022-12-03 16:33:07 +0900

    [level 2] Title: 전화번호 목록, Time: 78.86 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,24 +1,39 @@
-def solution(phone_book):
-    # answer = True
-    # for num1 in phone_book:
-    #     for num2 in phone_book:
-    #         if num2 == num1:
-    #             continue
-    #         if num2[:len(num1)] == num1:
-    #             answer = False
-    #             return answer
-    # return answer
+# def solution(phone_book):
+#     # answer = True
+#     # for num1 in phone_book:
+#     #     for num2 in phone_book:
+#     #         if num2 == num1:
+#     #             continue
+#     #         if num2[:len(num1)] == num1:
+#     #             answer = False
+#     #             return answer
+#     # return answer
     
-    answer = True
-    dic = {}
-    for num in phone_book:
-        dic[num] = 1
+#     answer = True
+#     dic = {}
+#     for num in phone_book:
+#         dic[num] = 1
         
-    for num in phone_book:
-        tmp = ''
-        for i in range(len(num)-1):
-            tmp += num[i]
-            if tmp in dic:
-                answer = False
-                return answer
-    return answer
+#     for num in phone_book:
+#         tmp = ''
+#         for i in range(len(num)-1):
+#             tmp += num[i]
+#             if tmp in dic:
+#                 answer = False
+#                 return answer
+#     return answer
+
+2
+3
+4
+5
+6
+7
+8
+def solution(phoneBook):
+    phoneBook = sorted(phoneBook)
+
+    for p1, p2 in zip(phoneBook, phoneBook[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
