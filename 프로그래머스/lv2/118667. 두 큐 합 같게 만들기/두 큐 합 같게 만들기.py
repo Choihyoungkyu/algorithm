07bbb202bddd953b145ad14173237396e5fbaef6
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2023-01-04 00:24:13 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 47.45 ms, Memory: 19.2 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -7,7 +7,10 @@ def solution(queue1, queue2):
     tot1 = sum(queue1)
     tot2 = sum(queue2)
     cnt = 0
-    L = 3*len(queue1)
+    L = 3*len(queue1)           # que1과 que2의 길이가 같고 양쪽 전부 다 옮길 때까지 반복
+                                # que1을 마지막 빼고 전부 que2로 옮긴 다음 다시 que2를 마지막 빼고 전부 que1으로 옮길 경우
+                                # que1 -> que2 : l
+                                # que2 -> que1 : 2*l
     
     while cnt < L:
         if tot1 == tot2:
