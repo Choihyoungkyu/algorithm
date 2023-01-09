30eb2bcd46b273778f2773e912f180ce733ab919
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2023-01-10 00:27:41 +0900

    [level 2] Title: 후보키, Time: 0.15 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42890. 후보키/후보키.py
@@ -0,0 +1,40 @@
+from itertools import combinations
+
+def solution(relation):
+    answer = 0
+    idxs = [i for i in range(len(relation[0]))]
+    result_lst = []
+    # 유일성 구하기
+    for i in range(1, len(idxs)+1):
+        for combination in combinations(idxs, i):
+            tmp_dic = {}  
+            for a in relation:
+                tmp_str = ""
+                for idx in combination:
+                    tmp_str += a[idx]
+                if tmp_str not in tmp_dic:
+                    tmp_dic[tmp_str] = 1
+                else:
+                    break
+            else:
+                tmp_str = ""
+                for j in combination:
+                    tmp_str += str(j)
+                result_lst.append(tmp_str)
+    # print(result_lst)
+    res = []
+    for i in result_lst:
+        if not res:
+            res.append(i)
+            answer += 1
+            continue
+        for j in res:
+            for k in j:
+                if k not in i:
+                    break
+            else:
+                break
+        else:
+            res.append(i)
+            answer += 1   
+    return answer
