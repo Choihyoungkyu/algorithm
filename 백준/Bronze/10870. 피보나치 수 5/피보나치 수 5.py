2104ad8bbbc29fa52699f8fa3bbc1959430358f4
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2023-01-22 23:57:36 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,7 +1,10 @@
-n = int(input())
+import sys
+input = lambda:sys.stdin.readline().strip()
 
-fibonacci = [0, 1]
-for i in range(2, n+1):
-    num = fibonacci[i-1] + fibonacci[i-2]
-    fibonacci.append(num)
-print(fibonacci[n])
+N = int(input())
+DP = [0] * 21
+DP[0] = 0
+DP[1] = 1
+for i in range(2, 21):
+    DP[i] = DP[i-2] + DP[i-1]
+print(DP[N])
