ad1f00a54e9805628a6a1920182a8df3008ec573
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2023-02-01 22:20:42 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 123.49 ms, Memory: 32 MB -BaekjoonHub

--- 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -4,7 +4,9 @@ def solution(gems):
     start = 0
     end = -1
     tmp = {}
+    
     while True:
+        # 조건 만족 X => 끝점 이동
         if len(tmp) != len(gems_set):
             end += 1
             if end < len(gems):
@@ -12,16 +14,17 @@ def solution(gems):
                     tmp[gems[end]] = 0
                 tmp[gems[end]] += 1
                 
-            
+        # 조건 만족 O => 시작점 이동
         elif len(tmp) == len(gems_set):
-            answer.append([abs(end-start), start+1, end+1])
+            answer.append([end-start, start+1, end+1])
             tmp[gems[start]] -= 1
             if tmp[gems[start]] == 0:
                 del tmp[gems[start]]
             start += 1
+            
+        # 종료 조건
         if start == len(gems) or end == len(gems):
             break
-        # print(tmp)
+            
     answer.sort()
-    # print(answer)
     return answer[0][1:]
