a87a2ea024721c63b531960d70fb34d2a7331049
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2023-02-06 22:53:33 +0900

    [level 3] Title: 징검다리 건너기, Time: 211.06 ms, Memory: 18.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -0,0 +1,19 @@
+def solution(stones, k):
+    start = 0
+    end = 200000000
+    while start <= end:
+        mid = (start + end) // 2
+        cnt = 0
+        for stone in stones:
+            if stone - mid <= 0:
+                cnt += 1
+            else:
+                cnt = 0
+            if cnt >= k:
+                break
+        if cnt >= k:
+            end = mid - 1
+        else:
+            start = mid + 1
+        
+    return start
