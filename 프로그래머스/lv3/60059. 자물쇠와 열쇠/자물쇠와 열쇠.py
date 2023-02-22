a72eb65c8c60f09aa21324d9f0125282b5440f44
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2023-02-22 22:09:47 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 0.24 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
+++ 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
@@ -1,3 +1,4 @@
+# key를 오른쪽으로 돌리기
 def rotate_key(key):
     res = []
     for j in range(len(key)):
@@ -7,13 +8,17 @@ def rotate_key(key):
         res.append(tmp[:])
     return res
 
+# 자물쇠 풀기 시도 
+# (자물쇠 해당 위치 == key의 오른쪽 밑) => key의 크기만큼 다 돌림
 def unlock(i, j, key, lock):
     key_i, cnt = 0, 0
     for ti in range(i-len(key)+1, i+1):
         key_j = 0
         for tj in range(j-len(key)+1, j+1):
+            # 홈을 채우면 카운트 +1
             if 0<=ti<len(lock) and 0<=tj<len(lock) and lock[ti][tj] == 0 and key[key_i][key_j] == 1:
                 cnt += 1
+            # 홈이 아닌데 key의 홈이 닿으면 ㅈㅈ
             elif 0<=ti<len(lock) and 0<=tj<len(lock) and lock[ti][tj] == 1 and key[key_i][key_j] == 1:
                 return 0
             key_j += 1
@@ -23,21 +28,19 @@ def unlock(i, j, key, lock):
 def solution(key, lock):
     answer = False
     zeros = 0
+    # 자물쇠의 홈 부분 개수 세기
     for i in range(len(lock)):
         for j in range(len(lock)):
             if not lock[i][j]:
                 zeros += 1
-            
-    cnt = 0
+    
+    # key 모양 4번 * 자물쇠 전체 크기
     for _ in range(4):
         for i in range(len(lock)+len(key)-1):
             for j in range(len(lock)+len(key)-1):
-                cnt += 1
                 if zeros == unlock(i, j, key, lock):
-                    # print(cnt, i, j, key)
                     return True
                 
-        
         key = rotate_key(key)
     
     return answer
