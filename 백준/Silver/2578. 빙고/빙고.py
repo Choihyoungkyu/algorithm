4301602f93ebc357024a44f991e125905748aff8
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2023-04-09 23:17:34 +0900

    [Silver IV] Title: 빙고, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.py
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -1,10 +1,10 @@
 import sys
 input = lambda: sys.stdin.readline().strip()
 
-bingo = [list(map(int, input().split())) for _ in range(5)]
-cnt = 0
-bing = 0
-lst = [[],[],[],[]] # 이미 체크한 줄(가로, 세로, 대각1, 대각2)
+bingo = [list(map(int, input().split())) for _ in range(5)]  # 빙고 리스트
+cnt = 0  # 수의 순서
+bing = 0  # bingo 수
+lst = [[],[],[],[]] # 이미 체크한 줄 입력(가로, 세로, 대각1, 대각2)
 
 flag = False
 for _ in range(5):
@@ -12,9 +12,9 @@ for _ in range(5):
     for k in call:    
         for i in range(5):
             if k in bingo[i]:
-                tmp = bingo[i].index(k)
-                bingo[i][tmp] = 0
-                cnt += 1
+                tmp = bingo[i].index(k)             # 열의 인덱스 저장
+                bingo[i][tmp] = 0                   # 불러진 수를 0으로 바꿈
+                cnt += 1                            # 수의 개수 증가
                 
                 for j in range(5):                  # 가로
                     if bingo[i][j] == 0:
@@ -22,10 +22,10 @@ for _ in range(5):
                     else:
                         break
                 else:
-                    if i not in lst[0]:
+                    if i not in lst[0]:             # 이미 체크한 빙고 제외
                         bing += 1
                         lst[0].append(i)
-                    if bing >= 3:
+                    if bing >= 3:                   # 빙고가 3개 이상이면 끝
                         flag = True
                         break
 
@@ -35,10 +35,10 @@ for _ in range(5):
                     else:
                         break
                 else:
-                    if tmp not in lst[1]:
+                    if tmp not in lst[1]:           # 이미 체크한 빙고 제외
                         bing += 1
                         lst[1].append(tmp)
-                    if bing >= 3:
+                    if bing >= 3:                   # 빙고가 3개 이상이면 끝
                         flag = True
                         break
 
@@ -48,10 +48,10 @@ for _ in range(5):
                     else:
                         break
                 else:
-                    if 0 not in lst[2]:
+                    if 0 not in lst[2]:             # 이미 체크한 빙고 제외
                         bing += 1
                         lst[2].append(0)
-                    if bing >= 3:
+                    if bing >= 3:                   # 빙고가 3개 이상이면 끝
                         flag = True
                         break
 
@@ -61,10 +61,10 @@ for _ in range(5):
                     else:
                         break
                 else:
-                    if 0 not in lst[3]:
+                    if 0 not in lst[3]:             # 이미 체크한 빙고 제외
                         bing += 1
                         lst[3].append(0)
-                    if bing >= 3:
+                    if bing >= 3:                   # 빙고가 3개 이상이면 끝
                         flag = True
                         break
 
@@ -74,5 +74,4 @@ for _ in range(5):
             break
     if flag == True:
         break
-
 print(cnt)
