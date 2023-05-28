2495c1debc38414926d00361e89bd426887d307c
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2023-05-28 20:02:42 +0900

    [Silver II] Title: 최대 힙, Time: 332 ms, Memory: 34692 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -1,41 +1,38 @@
 import sys
 input = lambda:sys.stdin.readline().strip()
 
-# 최대힙 원소 넣기
 def enq(n):
     global last
-    last += 1               # 마지막 정점 추가
-    heap[last] = n          # 마지막 정점에 key 추가
-    c = last                # 마지막 정점 번호
-    p = c // 2              # 완전이진트리에서 부모 정점 번호
+    last += 1
+    heap[last] = n
+    c = last
+    p = c // 2
 
-    # 부모가 있고, 부모 < 자식인 경우 자리 교환
     while p and heap[c] > heap[p]:
         heap[p], heap[c] = heap[c], heap[p]
         c = p
         p = c // 2
 
-# 최대힙의 루트 출력
 def deq():
     global last
-    if last == 0:                                       # heap이 비어있으면
-        return 0                                        # 0 반환
-    else:                                               # heap이 비어있지 않으면
-        tmp = heap[1]                                   # 루트 백업
-        heap[1] = heap[last]                            # 삭제할 노드의 키를 루트에 복사
-        last -= 1                                       # 마지막 노드 삭제
-        p = 1                                           # 루트에 옮긴 값을 자식과 비교
-        c = 2 * p                                       # 왼쪽 자식과 비교
-        while c <= last:                                # 자식이 하나라도 있으면
-            if c+1 <= last and heap[c] < heap[c+1]:     # 오른쪽 자식이 왼쪽 자식보다 크면
-                c += 1                                  # 비교 대상을 오른쪽 자식으로 정함
-            if heap[p] < heap[c]:                       # 자식이 부모보다 크면
-                heap[p], heap[c] = heap[c], heap[p]     # 교환
-                p = c                                   # 자식을 새로운 부모로
-                c = p * 2                               # 왼쪽 자식 번호를 계산
-            else:                                       # 부모가 더 크면
-                break                                   # 비교 중단
-        return tmp                                      # 백업한 최대값 반환
+    if last == 0:
+        return 0
+    else:
+        tmp = heap[1]
+        heap[1] = heap[last]
+        last -= 1
+        p = 1
+        c = 2 * p
+        while c <= last:
+            if c+1 <= last and heap[c] < heap[c+1]:
+                c += 1
+            if heap[p] < heap[c]:
+                heap[p], heap[c] = heap[c], heap[p]
+                p = c
+                c = p * 2
+            else:
+                break
+        return tmp
 
 E = int(input())
 heap = [0] * (E+1)
