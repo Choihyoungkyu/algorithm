7d18c08131a1a57ae8719bb4489d5aef82796ef8
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2023-06-01 15:34:10 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -4,29 +4,25 @@ input = lambda:sys.stdin.readline().strip()
 
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
+memo = [list([0, 0, 0] for _ in range(N)) for _ in range(N)]
+# print(memo)
+memo[0][1][0] = 1
 
-# status -> -1 : 가로, 0 : 대각선, 1 : 세로
+# status -> 0 : 가로, 1 : 대각선, 2 : 세로
 
-def dfs(arr, i, j, status):
-    answer = 0
-    # 오른쪽으로 밀 경우
-    if (status == -1 or status == 0) and j+1 < N and not arr[i][j+1]:
-        if i == N-1 and j+1 == N-1:
-            return 1
-        if j+1 != N-1:
-            answer += dfs(arr, i, j+1, -1)
-    # 아래로 밀 경우
-    if (status == 1 or status == 0) and i+1 < N and not arr[i+1][j]:
-        if i+1 == N-1 and j == N-1:
-            return 1
-        if i+1 != N-1:
-            answer += dfs(arr, i+1, j, 1)
-    # 대각선으로 밀 경우
-    if i+1 < N and j+1 < N and not arr[i][j+1] and not arr[i+1][j+1] and not arr[i+1][j]:
-        if i+1 == N-1 and j+1 == N-1:
-            return 1
-        answer += dfs(arr, i+1, j+1, 0)
-    return answer
+for i in range(N):
+    for j in range(1, N):
+        if arr[i][j]:
+            continue
+        # 가로 = 왼쪽에 가로, 대각선으로 놓여있는 경우
+        memo[i][j][0] += memo[i][j-1][0] + memo[i][j-1][1]
 
-ans = dfs(arr, 0, 1, -1)
-print(ans)
+        if i == 0: continue
+        # 대각선 = 왼쪽 위에 가로, 세로, 대각선으로 놓여있는 경우
+        if not arr[i-1][j-1] and not arr[i][j-1] and not arr[i-1][j]:
+            memo[i][j][1] += sum(memo[i-1][j-1])
+
+        # 세로 = 위에 대각선, 세로로 놓여있는 경우
+        memo[i][j][2] += memo[i-1][j][1] + memo[i-1][j][2]
+
+print(sum(memo[N-1][N-1]))
