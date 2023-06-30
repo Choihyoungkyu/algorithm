b51048f8072bb65c1509d15f3eeb6929ec2155e0
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2023-06-30 18:24:22 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 2196 ms, Memory: 117452 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,49 +1,36 @@
-import sys
 from collections import deque
-input = lambda:sys.stdin.readline().strip()
+import sys
+input = sys.stdin.readline
 
 N, M = map(int, input().split())
-arr = [list(input()) for _ in range(N)]
-D = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+Map = [list(input().strip()) for _ in range(N)]
 
-visited = [[0]*M for _ in range(N)]
-visited2 = [[0]*M for _ in range(N)]
-visited[0][0] = 1
-ans = -1
+def BFS():
+    D = ((0, 1), (0, -1), (1, 0), (-1, 0))
+    visited_possible = [[0] * M for _ in range(N)]
+    visited_impossible = [[0] * M for _ in range(N)]
+    visited_possible[0][0] = 1
+    queue = deque()
+    queue.append((0, 0, True))
+    while queue:
+        ci, cj, is_possible = queue.popleft()
+        if ci == N-1 and cj == M-1:
+            return max(visited_possible[ci][cj], visited_impossible[ci][cj])
+        for di, dj in D:
+            ni, nj = ci+di, cj+dj
+            if is_possible:
+                if 0<=ni<N and 0<=nj<M and not visited_possible[ni][nj]:
+                    if Map[ni][nj] == '0':
+                        visited_possible[ni][nj] = visited_possible[ci][cj]+1
+                        queue.append((ni, nj, is_possible))
+                    else:
+                        visited_impossible[ni][nj] = visited_possible[ci][cj]+1
+                        queue.append((ni, nj, False))
+            else:
+                if 0<=ni<N and 0<=nj<M and not visited_impossible[ni][nj] and Map[ni][nj] == '0':
+                    visited_impossible[ni][nj] = visited_impossible[ci][cj]+1
+                    queue.append((ni, nj, is_possible))
 
-que = deque()
-que.append([0, 0, 0])
-while que:
-    ci, cj, flag = que.popleft()
-    for di, dj in D:
-        ni, nj = ci+di, cj+dj
-        if flag:
-            if 0<=ni<N and 0<=nj<M and (visited2[ni][nj]==0 or visited2[ni][nj]>visited2[ci][cj]+1) and arr[ni][nj]=='0':
-                que.append([ni, nj, flag])
-                visited2[ni][nj] = visited2[ci][cj]+1
-        else:
-            if 0<=ni<N and 0<=nj<M and (visited[ni][nj]==0 or visited[ni][nj]>visited[ci][cj]+1):
-                if arr[ni][nj] == '1':
-                    que.append([ni, nj, flag+1])
-                    visited2[ni][nj] = visited[ci][cj]+1
-                else:
-                    que.append([ni, nj, flag])
-                    visited[ni][nj] = visited[ci][cj]+1
 
-    if visited[N-1][M-1]:
-        ans = visited[N-1][M-1]
-        break
-    if visited2[N-1][M-1]:
-        ans = visited2[N-1][M-1]
-        break
-
-# for i in arr:
-#     print(i)
-# print()
-# for i in visited:
-#     print(i)
-# print()
-# for i in visited2:
-#     print(i)
-# print()
-print(ans)
+ans = BFS()
+print(ans if ans else -1)
