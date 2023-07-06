b6ed84f4ac42fe40cc7cffa9a567d090c2a8f131
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2023-07-06 13:48:50 +0900

    [Silver I] Title: 스티커, Time: 912 ms, Memory: 49476 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -13,8 +13,10 @@ for tc in range(T):
     for i in range(1, N):
         if i == 1:
             DP[0][i], DP[1][i] = Numbers[0][i] + DP[1][i-1], Numbers[1][i] + DP[0][i-1]
-            continue
-        DP[0][i] = max(DP[1][i-1]+Numbers[0][i], DP[1][i-2]+Numbers[0][i])
-        DP[1][i] = max(DP[0][i-1]+Numbers[1][i], DP[0][i-2]+Numbers[1][i])
-        
-    print(max(max(DP[0]), max(DP[1])))
+        else:
+            DP[0][i] = max(DP[1][i-1]+Numbers[0][i], DP[1][i-2]+Numbers[0][i])
+            DP[1][i] = max(DP[0][i-1]+Numbers[1][i], DP[0][i-2]+Numbers[1][i])
+        if i == N-1:
+            max_v = max(DP[0][N-1], DP[1][N-1])
+
+    print(max_v)
