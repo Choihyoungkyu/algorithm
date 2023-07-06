5bbbcffbf33b5ad9bf4bfc790de4d05f5015c847
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2023-07-06 13:45:05 +0900

    [Silver I] Title: 스티커, Time: 924 ms, Memory: 49476 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -0,0 +1,20 @@
+import sys
+T = int(input())
+for tc in range(T):
+    N = int(input())
+    Numbers = [list(map(int, input().split())) for _ in range(2)]
+    DP = [[0] * N for _ in range(2)]
+    DP[0][0], DP[1][0] = Numbers[0][0], Numbers[1][0]
+    
+    if N == 1:
+        print(max(DP[0][0], DP[1][0]))
+        continue
+
+    for i in range(1, N):
+        if i == 1:
+            DP[0][i], DP[1][i] = Numbers[0][i] + DP[1][i-1], Numbers[1][i] + DP[0][i-1]
+            continue
+        DP[0][i] = max(DP[1][i-1]+Numbers[0][i], DP[1][i-2]+Numbers[0][i])
+        DP[1][i] = max(DP[0][i-1]+Numbers[1][i], DP[0][i-2]+Numbers[1][i])
+        
+    print(max(max(DP[0]), max(DP[1])))
