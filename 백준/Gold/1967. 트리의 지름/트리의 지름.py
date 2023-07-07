f2ab046ada9bbab9eef2ab10ce99f278ad5d48cb
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2023-07-07 14:26:31 +0900

    [Gold IV] Title: 트리의 지름, Time: 92 ms, Memory: 37420 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -1,48 +1,27 @@
-import sys
-from collections import deque
+import sys, heapq
 input = sys.stdin.readline
 
 N = int(input())
 adjL = [[] for _ in range(N+1)]
 for _ in range(N-1):
     a, b, c = map(int, input().split())
-    adjL[a].append([c, b])
+    adjL[a].append((-c, b))
+    adjL[b].append((-c, a))
 
-Tree = [[0, 0] for _ in range(N+1)]
-maxV = 0
+def dijkstra(i):
+    queue = []
+    heapq.heappush(queue, (0, i))
+    visited = [1] * (N+1)
+    while queue:
+        cost, idx = heapq.heappop(queue)
+        if visited[idx] != 1:
+            continue
+        visited[idx] = cost
+        for c, i in adjL[idx]:
+            if visited[i] == 1:
+                heapq.heappush(queue, (cost + c, i))
+    return visited
 
-new_adjL = [[] for _ in range(N+1)]
-visited = [0] * (N+1)
-que = deque()
-que.append(1)
-dic = {}
-while que:
-    idx = que.popleft()
-    if visited[idx]: continue
-
-    visited[idx] = 1
-    for i in range(1, N+1):
-        if not new_adjL[i]:
-            new_adjL[i] = adjL[idx]
-            dic[idx] = i
-            break
-
-    for c, i in adjL[idx]:
-        if not visited[i]:
-            que.append(i)
-
-def func(idx, maxV):
-    tmp = 0
-    lst = []
-    for c, i in new_adjL[idx]:
-        i = dic[i]
-        c += Tree[i][1]
-        lst.append(c)
-        tmp = max(tmp, c)
-    lst.sort(reverse=True)
-    Tree[idx] = [sum(lst[:2]), tmp]
-    return max(sum(lst[:2]), tmp, maxV)
-
-for i in range(N, 0, -1):
-    maxV = func(i, maxV)
-print(maxV)
+answer = 0
+arr1 = dijkstra(1)
+print(-min(dijkstra(arr1.index(min(arr1)))))
