ad6a365e7fed603eaec1eb84dfe9d6da12e0781e
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2023-10-23 16:43:08 +0900

    [level 3] Title: 가장 먼 노드, Time: 27.57 ms, Memory: 20 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def solution(n, edge):
+    answer = 0
+    adjL = [[] for _ in range(n+1)]
+    for a, b in edge:
+        adjL[a].append(b)
+        adjL[b].append(a)
+
+    visited = [float('inf')] * (n+1)
+    visited[0] = 0
+    visited[1] = 0
+    idx_lst = deque()
+    idx_lst.append(1)
+
+    while idx_lst:
+        idx = idx_lst.popleft()
+        for i in adjL[idx]:
+            if visited[i] > visited[idx] + 1:
+                visited[i] = visited[idx] + 1
+                idx_lst.append(i)
+    # print(visited)
+    answer = visited.count(max(visited))
+    return answer
