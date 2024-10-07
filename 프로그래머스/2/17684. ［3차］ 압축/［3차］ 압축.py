3ce7ca7de3438c61ec10956f61676c8fcaea9b30
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2024-10-08 00:12:14 +0900

    [level 2] Title: [3차] 압축, Time: 1.15 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -0,0 +1,56 @@
+def solution(msg):
+    answer = []
+    idx = 26
+    dic = {"A": 1,
+           "B": 2,
+           "C": 3,
+           "D": 4,
+           "E": 5,
+           "F": 6,
+           "G": 7,
+           "H": 8,
+           "I": 9,
+           "J": 10,
+           "K": 11,
+           "L": 12,
+           "M": 13,
+           "N": 14,
+           "O": 15,
+           "P": 16,
+           "Q": 17,
+           "R": 18,
+           "S": 19,
+           "T": 20,
+           "U": 21,
+           "V": 22,
+           "W": 23,
+           "X": 24,
+           "Y": 25,
+           "Z": 26
+          }
+    
+    i = 0
+    while i < len(msg):
+        flag = False
+        jj = i+1
+        
+        if msg[i:] in dic:
+            answer.append(dic[msg[i:]])
+            break
+            
+        for j in range(i+1, len(msg)+1):
+            if msg[i:j] not in dic:
+                flag = True
+                jj = j
+                break
+        
+        if flag:
+            idx += 1
+            dic[msg[i:jj]] = idx
+            answer.append(dic[msg[i : jj-1]])
+            i = jj-1
+        else:
+            answer.append(dic[msg[i : jj]])
+            i += 1
+            
+    return answer
